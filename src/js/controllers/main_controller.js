df698a1027427a2f2541b01c2e3e9320941c35e2
Jorge Davison <jdavisonc@gmail.com>
2015-04-20 13:35:59 -0300

    Add config object

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -1,5 +1,46 @@
 angular.module('Rasparmony.controllers.Main', [])
 
 .controller('MainController', function($scope){
+
+	$scope.config = {
+		macros: [
+			{ 
+				"name": "TV", 
+				"commands": [ 
+					{ "remote": "TV", "command": "power" }, 
+					{ "remote": "HomeTheater", "command": "power" } 
+				] 
+			},
+			{ 
+				"name": "Nexus", 
+				"commands": [ 
+					{ "remote": "TV", "command": "input" }, 
+					{ "remote": "TV", "command": "input" }, 
+					{ "remote": "TV", "command": "input" }
+				] 
+			},
+			{ 
+				"name": "Cable", 
+				"commands": [ 
+					{ "remote": "TV", "command": "input" }, 
+					{ "remote": "TV", "command": "input" }, 
+					{ "remote": "TV", "command": "input" }
+				] 
+			},
+			{ 
+				"name": "Radio", 
+				"commands": [ 
+					{ "remote": "HomeTheater", "command": "power" },
+					{ "remote": "HomeTheater", "command": "input" },
+					{ "remote": "HomeTheater", "command": "input" },
+					{ "remote": "HomeTheater", "command": "input" }
+				] 
+			}
+		],
+		remotes: [ 
+			{ "name": "TV", code: "LHV4420" }, 
+			{ "name": "HomeTheater", code: "888888" }
+		]
+	};
   
 });
