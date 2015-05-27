41e09a4f73954c34cc587b879dc27cc40eb296a0
jdavisonc <jdavisonc@gmail.com>
2015-05-26 21:32:03 -0300

    Add ctrl+c capture and setup lircd confgiuration

--- findCodes.sh
+++ findCodes.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
-INSTALLATION_PATH=/tmp
+#
+# Script to configure a device remote code on lircd installation.
+#
+# Requirements: git lircd
+# Author: Jorge Davison
 
-cd $INSTALLATION_PATH
+INSTALLATION_PATH=/tmp
+CODES_DIR=$INSTALLATION_PATH/lirc-remotes-code/remotes/
+LIRCD_CONF_PATH=/etc/lirc/lircd.conf
+
+_term() {
+	cat $LIRCD_CONF_PATH >> $LIRCD_CONF_PATH.bck
+	rm $LIRCD_CONF_PATH
+	cp $LIRCD_CONF_PATH.bck $LIRCD_CONF_PATH
+
+	popd
+}
+# trap keyboard interrupt (control-c)
+trap _term SIGINT
+
+# Backup lircd conf
+cp $LIRCD_CONF_PATH $LIRCD_CONF_PATH.bck
+
+pushd $INSTALLATION_PATH
 
 git clone git://git.code.sf.net/p/lirc-remotes/code lirc-remotes-code
-CODES_DIR=$INSTALLATION_PATH/lirc-remotes-code/remotes/
-
 cd $CODES_DIR
 
 printf "Please select the device brand:\n"
 select brand in */; do test -n "$brand" && break; echo ">>> Invalid Selection"; done
 cd "$brand"
 
+echo "Iteration will begin, please break the script 'Ctrl+C' when your device shutdown"
+sleep 2
+
 for filename in *.conf; do
 	CODE_NAME=$(sed -ne "s/\s*name\s*\(\d*\)\s*/\1/p" $filename)
 	echo "Testing with file '$filename' (code '$CODE_NAME')..."
 
 	rm /etc/lirc/lircd.conf
-	ln -s $CODES_DIR/$brand/$filename /etc/lirc/lircd.conf
+	ln -s $CODES_DIR/$brand/$filename $LIRCD_CONF_PATH
 	/etc/init.d/lirc restart
 
 	echo "Sending KEY_POWER with $CODE_NAME..."
 	irsend SEND_ONCE $CODE_NAME KEY_POWER
-	echo "Command send, please break the script when you are satisfied"
 	sleep 10
 done
-
-#TODO
-# Capture Ctrl+C and create lircd file or append to the existent one
